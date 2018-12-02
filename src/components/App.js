670443c42ae4e5f143a24f6e9f1d5a896de45257
Tomasz Chowaniec <chowaniec.tomek@gmail.com>
2018-12-02 19:37:01 +0100

    removed expense data

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,6 @@ import AppRouter from '../routers/AppRouter';
 
 import configureStore from '../store/configureStore';
 
-import { addExpense } from '../actions/expenses';
-import { setTextFilter } from '../actions/filters';
-import getVisibleExpenses from '../selectors/expenses';
-
 const store = configureStore();
 
 export default class App extends Component {
